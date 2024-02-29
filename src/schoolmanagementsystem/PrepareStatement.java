699e936783152283a8a2c4f7fbaef774a9150f2c
gobalakrishnan21cb17 <kbaskarankbaskaran72@gmail.com>
2024-02-29 12:08:10 +0530

    Updated

---
+++ src/schoolmanagementsystem/PrepareStatement.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package schoolmanagementsystem;
+
+/**
+ *
+ * @author GOBALA KRISHNAN
+ */
+class PrepareStatement {
+
+    void setString(int i, String text) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    void executeUpdate() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    void setDouble(int i, double a) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    void setDouble(int i, String mn) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
---
+++ src/schoolmanagementsystem/conn.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package schoolmanagementsystem;
+
+/**
+ *
+ * @author GOBALA KRISHNAN
+ */
+class conn {
+
+    static void close() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
